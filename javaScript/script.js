1035df8f249ced5ba3c69b7286fee93861f91a90
Antonio Estevam da Silva Filho(Jhonny) <47607852+Jhonny3x4@users.noreply.github.com>
2020-05-07 17:57:06 -0300

    Criação e estilização Front End

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,12 +1,24 @@
 const $menu = document.querySelector('.menu');
 const $btnMenuOpen = document.querySelector('.btnMenu_open');
 const $btnMenuClose = document.querySelector('.btnMenu_close');
+const $btnDownload = document.querySelector('#bt_download');
+const $menu_item_action = document.getElementsByClassName('menu-item-action');
 
-$btnMenuOpen.addEventListener('click',()=>{
-    console.log($menu);    
+$btnMenuOpen.addEventListener('click',()=>{ 
     $menu.classList.add('menu_open');
 });
 
 $btnMenuClose.addEventListener('click',()=>{
     $menu.classList.remove('menu_open');
 });
+
+$btnDownload.addEventListener('click',()=>{
+    alert("No momento essa opção não esta disponiver!!");
+});
+    //Esconder menu apos clicado 
+for(let i=0; i < $menu_item_action.length; i++){
+    $menu_item_action[i].addEventListener('click',()=>{
+        $menu.classList.remove('menu_open');
+    });
+    
+}
